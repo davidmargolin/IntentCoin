9bd1f8d0134a21afb240bbf6b20ce81e40ebe15e
David Margolin <davidmargolin0305@gmail.com>
2018-03-24 03:59:59 -0400

    mixed

--- healthblock/src/App.js
+++ healthblock/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from "./components/NavBar"
-
+import ExploreList from "./components/ExploreList"
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
+      <div>
+        <ExploreList />
+    </div>
+
       </div>
     );
   }
--- healthblock/src/components/NavBar.js
+++ healthblock/src/components/NavBar.js
@@ -6,12 +6,14 @@ export default class NavBar extends Component{
     return (
   <div>
     <div className="Nav-Bar">
+      <div className="NavLeft">
       <img className="Logo" src = "https://i.pinimg.com/originals/3c/eb/9a/3ceb9ae6a1040db7fe5b5f35fcd05aac.jpg">
       </img>
       <h1 className="Name">
         CharityBlock
       </h1>
-      <input type="text" placeholder="Search" className="search-bar"/>
+      <input type="text" placeholder="Search Charities..." className="search-bar"/>
+      </div>
       <ul className="project-button">
         <li><a href = "">Make a project</a></li>
       </ul>
