79e36219dcbd0b29082d7703ad38b5a087e7f48f
Deren <derenlin123456@gmail.com>
2018-03-24 03:42:18 -0400

    NavBar

--- healthblock/src/App.js
+++ healthblock/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
+import NavBar from "./components/NavBar"
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar/>
       </div>
     );
   }
---
+++ healthblock/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+export default class NavBar extends Component{
+  render() {
+    return (
+  <div>
+    <div className="Nav-Bar">
+      <img className="Logo" src = "https://i.pinimg.com/originals/3c/eb/9a/3ceb9ae6a1040db7fe5b5f35fcd05aac.jpg">
+      </img>
+      <h1 className="Name">
+        CharityBlock
+      </h1>
+      <input type="text" placeholder="Search" className="search-bar"/>
+      <ul className="project-button">
+        <li><a href = "">Make a project</a></li>
+      </ul>
+    </div>
+  </div>
+)
+}
+}
