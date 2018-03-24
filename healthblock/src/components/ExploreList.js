81b32209cc3107cea64cf25570e17cec7bafafc6
David Margolin <davidmargolin0305@gmail.com>
2018-03-24 18:17:10 -0400

    rearranged

--- healthblock/src/components/ExploreList.js
+++ healthblock/src/components/ExploreList.js
@@ -5,8 +5,9 @@ export default class ExploreList extends Component {
   render() {
     return (
       <div style={{padding: 20}}>
-        <CardSection image_list={this.props.health} title="Health"/>
         <CardSection image_list={this.props.environment} title="Environment"/>
+        <CardSection image_list={this.props.health} title="Health"/>
+
       <CardSection image_list={this.props.education} title="Policy"/>
       </div>
     );
