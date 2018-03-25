525fa85e3bd8158bf726a52bb9272b5ab3e535d5
David Margolin <davidmargolin0305@gmail.com>
2018-03-25 09:54:21 -0400

    product shots

--- healthblock/src/components/NavBar.js
+++ healthblock/src/components/NavBar.js
@@ -8,7 +8,7 @@ export default class NavBar extends Component{
     <div style={{width: '100%', backgroundColor: 'white', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', boxShadow: '1px 1px 1px #cEcEcE'}}>
       <div style={{backgroundColor: 'white', display:'flex', flexDirection: 'row', alignItems: 'center', flex:1}} >
         <Link to="/"style={{textDecoration: 'none', color: 'black', display: 'flex', flexDirection: 'row'}}>
-      <h2 style={{flex:1, margin: 20, color: '#6A6FEA'}}>CharityBlock</h2></Link>
+      <h2 style={{flex:1, margin: 20, color: '#6A6FEA'}}>Intention</h2></Link>
     <input type="text" placeholder="Search Causes..." style={{height: 30,flex:1,borderradius: 0,maxWidth: 800, fontSize: 15,padding: 8,margin: 20}}/>
       </div>
         <button style={{width: 150,padding: 6, marginRight: 24, color: '#6A6FEA', fontSize: '12'}} href = "/">List Your Cause</button>
